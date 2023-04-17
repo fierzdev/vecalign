8a6efe4683ba112f765ffa752d75568792ad2746
fierzdev <fierz@textshuttle.ai>
2023-04-17 03:33:56 +0200

    Make function for alignment

--- vecalign/vecalign.py
+++ vecalign/vecalign.py
@@ -85,49 +85,54 @@ def _main():
                         help='Write stack to pickle file for debug purposes')
 
     args = parser.parse_args()
+    align(args.src, args.tgt, args.src_embed, args.tgt_embed, args.gold_alignment, args.alignment_max_size, args.search_buffer_size, args.del_percentile_frac, args.max_size_full_dp,
+          args.costs_sample_size, args.num_samps_for_norm, args.verbose, args.debuge_save_stack)
 
-    if len(args.src) != len(args.tgt):
+
+def align(src, tgt, src_embed, tgt_embed, gold_alignment=None, alignment_max_size=4, search_buffer_size=5, del_percentile_frac=0.2, max_size_full_dp=300, costs_sample_size=20000,
+          num_samps_for_norm=100, verbose=False, debug_save_stack=None):
+    if len(src) != len(tgt):
         raise Exception('number of source files must match number of target files')
 
-    if args.gold_alignment is not None:
-        if len(args.gold_alignment) != len(args.src):
+    if gold_alignment is not None:
+        if len(gold_alignment) != len(src):
             raise Exception('number of gold alignment files, if provided, must match number of source and target files')
 
-    if args.verbose:
+    if verbose:
         import logging
         logger.setLevel(logging.INFO)
 
-    if args.alignment_max_size < 2:
+    if alignment_max_size < 2:
         logger.warning('Alignment_max_size < 2. Increasing to 2 so that 1-1 alignments will be considered')
-        args.alignment_max_size = 2
+        alignment_max_size = 2
 
-    src_sent2line, src_line_embeddings = read_in_embeddings(args.src_embed[0], args.src_embed[1])
-    tgt_sent2line, tgt_line_embeddings = read_in_embeddings(args.tgt_embed[0], args.tgt_embed[1])
+    src_sent2line, src_line_embeddings = read_in_embeddings(src_embed[0], src_embed[1])
+    tgt_sent2line, tgt_line_embeddings = read_in_embeddings(tgt_embed[0], tgt_embed[1])
 
-    width_over2 = ceil(args.alignment_max_size / 2.0) + args.search_buffer_size
+    width_over2 = ceil(alignment_max_size / 2.0) + search_buffer_size
 
     test_alignments = []
     stack_list = []
-    for src_file, tgt_file in zip(args.src, args.tgt):
+    for src_file, tgt_file in zip(src, tgt):
         logger.info('Aligning src="%s" to tgt="%s"', src_file, tgt_file)
 
         src_lines = open(src_file, 'rt', encoding="utf-8").readlines()
-        vecs0 = make_doc_embedding(src_sent2line, src_line_embeddings, src_lines, args.alignment_max_size)
+        vecs0 = make_doc_embedding(src_sent2line, src_line_embeddings, src_lines, alignment_max_size)
 
         tgt_lines = open(tgt_file, 'rt', encoding="utf-8").readlines()
-        vecs1 = make_doc_embedding(tgt_sent2line, tgt_line_embeddings, tgt_lines, args.alignment_max_size)
+        vecs1 = make_doc_embedding(tgt_sent2line, tgt_line_embeddings, tgt_lines, alignment_max_size)
 
-        final_alignment_types = make_alignment_types(args.alignment_max_size)
+        final_alignment_types = make_alignment_types(alignment_max_size)
         logger.debug('Considering alignment types %s', final_alignment_types)
 
         stack = vecalign(vecs0=vecs0,
                          vecs1=vecs1,
                          final_alignment_types=final_alignment_types,
-                         del_percentile_frac=args.del_percentile_frac,
+                         del_percentile_frac=del_percentile_frac,
                          width_over2=width_over2,
-                         max_size_full_dp=args.max_size_full_dp,
-                         costs_sample_size=args.costs_sample_size,
-                         num_samps_for_norm=args.num_samps_for_norm)
+                         max_size_full_dp=max_size_full_dp,
+                         costs_sample_size=costs_sample_size,
+                         num_samps_for_norm=num_samps_for_norm)
 
         # write final alignments to stdout
         print_alignments(stack[0]['final_alignments'], stack[0]['alignment_scores'])
@@ -135,13 +140,13 @@ def _main():
         test_alignments.append(stack[0]['final_alignments'])
         stack_list.append(stack)
 
-    if args.gold_alignment is not None:
-        gold_list = [read_alignments(x) for x in args.gold_alignment]
+    if gold_alignment is not None:
+        gold_list = [read_alignments(x) for x in gold_alignment]
         res = score_multiple(gold_list=gold_list, test_list=test_alignments)
         log_final_scores(res)
 
-    if args.debug_save_stack:
-        pickle.dump(stack_list, open(args.debug_save_stack, 'wb'))
+    if debug_save_stack:
+        pickle.dump(stack_list, open(debug_save_stack, 'wb'))
 
 
 if __name__ == '__main__':
